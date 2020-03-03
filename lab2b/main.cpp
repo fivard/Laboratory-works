97e1644c1a3da4429490ad2da16656cb8a754dd9
Dmytryi <57447810+fivard@users.noreply.github.com>
2020-03-03 22:28:27 +0200

    Create main.cpp

---
+++ lab2b/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "source.h"
+
+using namespace std;
+
+int main()
+{
+    bool isContinued = true;
+
+    while(isContinued){
+        cout << "Choose mode\n"
+             << "1 - Interactive\n"
+             << "2 - Demonstration\n"
+             << "3 - exit\n";
+        short action;
+        cin >> action;
+        switch (action){
+            case 1: 
+                break;
+            case 2: 
+                break;
+            case 3: isContinued = false;
+                break;
+            default: cout << "Choose correct number\n";
+        }
+    }
+    return 0;
+}
+
