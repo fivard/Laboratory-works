76c891ee4c53ca4c21d2a5346c4ee6a1f193d157
Dmytryi <57447810+fivard@users.noreply.github.com>
2020-03-24 19:26:57 +0200

    lab3b contains lab1 and  complements it
    complements it with 3 types of sort

--- lab3b/main.cpp
+++ lab3b/main.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include "message.h"
+#include "functions.h"
+#include "modes.h"
+#include <vector>
 
+using namespace std;
+
+int Message::count = 0;
+
+int main()
+{
+    bool check = true;
+
+    while(check){
+        cout << "Choose mode\n"
+             << "1 - Interactive\n"
+             << "2 - Demonstration\n"
+             << "3 - benchmark\n"
+             << "4 - exit\n";
+        short action;
+        cin >> action;
+        switch (action){
+            case 1: interactive();
+                break;
+            case 2: demonstration();
+                break;
+            case 3: benchmark();
+                break;
+            case 4: check = false;
+                break;
+            default: cout << "Choose correct number\n";
+        }
+    }
+    return 0;
+}
