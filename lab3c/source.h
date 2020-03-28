b0a10bdde192235cd7a3d09701ba606bba81f520
Dmytryi <57447810+fivard@users.noreply.github.com>
2020-03-29 00:44:59 +0200

    finished benchmark

--- lab3c/source.h
+++ lab3c/source.h
@@ -9,6 +9,7 @@
 #include <ctime>
 #include <vector>
 #include <stack>
+#include <algorithm>
 
 #define beginOfSubArray first
 #define sizeOfSubArray second
@@ -63,18 +64,19 @@ public:
 
         int i = 0;
         while(i < size) {
+            i++;
             if (i + minrun >= size){
                 solution.push_back({i, size - i});
                 insertionSort(i, size -1);
                 i = size;
             }else {
-                pair<int, int> newPair(i, minrun);
+                pair<int, int> newPair(i-1, minrun);
 
-                if (arr[i] >= arr[i + 1])
-                    while (arr[i] >= arr[i + 1] && i < size)
-                        i++;
-                else
-                    while (arr[i] < arr[i + 1] && i < size)
+//                if (arr[i-1] >= arr[i])
+//                    while (arr[i-1] >= arr[i] && i < size)   This part is conceived by author but if reversed array is so large, insertion sort's time is too much.
+//                        i++;                                 Test shows 9.35-12.35 sec instead 0.018 without this part. But may be, i did some mistake :)
+//                else
+                    while (arr[i-1] < arr[i] && i < size)
                         i++;
 
                 if (i - newPair.beginOfSubArray > minrun)
@@ -181,10 +183,158 @@ public:
         return {minBegin, minSize+maxSize};
     }
 
+    void benchTimSort(){
+        int minrun = GetMinrun();
+        benchMerge(benchSplit(minrun));
+    }
+    void benchInsertionSort(int left, int right) {
+        T tempValue;
+        for (int i = left + 1; i < right + 1; i++) {
+            tempValue = arr[i];
+            int j = i - 1;
+            while (tempValue < arr[j] && j >= left) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = tempValue;
+        }
+    }
+    vector<pair<int, int> > benchSplit(int minrun){
+        vector<pair<int, int> > solution;
+
+        int i = 0;
+        while(i < size) {
+            i++;
+            if (i + minrun >= size){
+                solution.push_back({i, size - i});
+                benchInsertionSort(i, size -1);
+                i = size;
+            }else {
+                pair<int, int> newPair(i-1, minrun);
+
+//                if (arr[i-1] >= arr[i])
+//                    while (arr[i-1] >= arr[i] && i < size)    This part is conceived by author but if reversed array is so large, insertion sort's time is too much.
+//                        i++;                                  Test shows 9.35-12.35 sec instead 0.018 without this part. But may be, i did some mistake :)
+//                else
+                    while (arr[i-1] < arr[i] && i < size)
+                        i++;
+
+                if (i - newPair.beginOfSubArray > minrun)
+                    newPair.sizeOfSubArray = i - newPair.beginOfSubArray;
+
+                solution.push_back(newPair);
+                benchInsertionSort(newPair.beginOfSubArray, newPair.beginOfSubArray + newPair.sizeOfSubArray-1);
+                i = newPair.beginOfSubArray + newPair.sizeOfSubArray;
+            }
+        }
+        return solution;
+    }
+    void benchMerge(vector<pair<int, int> > pointerAndSize){
+        vector<pair<int, int> > stack;
+
+        for (int i = 0; i < pointerAndSize.size(); i++){
+            stack.push_back(pointerAndSize[i]);
+            if (stack.size() > 2)
+                while (!(stack[stack.size()-3].sizeOfSubArray > stack[stack.size()-2].sizeOfSubArray + stack[stack.size()-1].sizeOfSubArray || stack[stack.size()-2].sizeOfSubArray > stack[stack.size()-1].sizeOfSubArray)){
+                    if (stack[stack.size()-2].sizeOfSubArray <= stack[stack.size()-1].sizeOfSubArray) {
+                        pair<int, int> newSubArray = benchMergeSubArray(stack[stack.size()-1], stack[stack.size()-2]);
+                        stack.pop_back();
+                        stack.pop_back();
+                        stack.push_back(newSubArray);
+                    }
+                    if (stack[stack.size()-3].sizeOfSubArray <= stack[stack.size()-2].sizeOfSubArray + stack[stack.size()-1].sizeOfSubArray)
+                        if (stack[stack.size()-3].sizeOfSubArray < stack[stack.size()-1].sizeOfSubArray){
+                            pair<int, int> last = stack[stack.size()-1];
+                            pair<int, int> newSubArray = benchMergeSubArray(stack[stack.size()-3], stack[stack.size()-2]);
+                            stack.pop_back();
+                            stack.pop_back();
+                            stack.pop_back();
+                            stack.push_back(newSubArray);
+                            stack.push_back(last);
+                        }else{
+                            pair<int, int> newSubArray = benchMergeSubArray(stack[stack.size()-1], stack[stack.size()-2]);
+                            stack.pop_back();
+                            stack.pop_back();
+                            stack.push_back(newSubArray);
+                        }
+                    else
+                        break;
+                }
+            if (stack.size() > 1)
+                if (stack[stack.size()-2].sizeOfSubArray <= stack[stack.size()-1].sizeOfSubArray) {
+                    pair<int, int> newSubArray = benchMergeSubArray(stack[stack.size()-1], stack[stack.size()-2]);
+                    stack.pop_back();
+                    stack.pop_back();
+                    stack.push_back(newSubArray);
+                }
+        }
+        while(stack.size() > 1){
+            pair<int, int> newSubArray = benchMergeSubArray(stack[stack.size()-1], stack[stack.size()-2]);
+            stack.pop_back();
+            stack.pop_back();
+            stack.push_back(newSubArray);
+        }
+    }
+    pair<int, int> benchMergeSubArray(pair<int, int> first, pair<int, int> second){
+        int minSize, maxSize, minBegin, maxBegin;
+        minBegin = min(first.beginOfSubArray, second.beginOfSubArray);
+        maxBegin = max(first.beginOfSubArray, second.beginOfSubArray);
+        if (minBegin == first.beginOfSubArray){
+            minSize = first.sizeOfSubArray;
+            maxSize = second.sizeOfSubArray;
+        }else{
+            minSize = second.sizeOfSubArray;
+            maxSize = first.sizeOfSubArray;
+        }
+
+        int temporary[minSize];
+        for (int i = 0; i < minSize; i++)
+            temporary[i] = arr[minBegin+i];
+
+        int temporaryPointer, subArrayPointer, arrayPointer;
+        temporaryPointer = 0;
+        subArrayPointer = maxBegin;
+        arrayPointer = minBegin;
+
+        while (temporaryPointer < minSize && subArrayPointer < maxBegin+maxSize) {
+            if (temporary[temporaryPointer] < arr[subArrayPointer])
+                arr[arrayPointer] = temporary[temporaryPointer++];
+            else
+                arr[arrayPointer] = arr[subArrayPointer++];
+            arrayPointer++;
+        }
+
+        while (temporaryPointer < minSize) {
+            arr[arrayPointer++] = temporary[temporaryPointer++];
+        }
+
+        return {minBegin, minSize+maxSize};
+    }
+
+    void librarySort(){
+        sort(arr, arr+size);
+    }
+
     void generateArray() {
         srand(time(nullptr));
         for (int i = 0; i < size; i++)
             arr[i] = rand();
+    }
+    void generateAlreadySorted(){
+        srand(time(nullptr));
+        for (int i = 0; i < size; i++)
+            arr[i] = i;
+        int count = rand()%10;
+        for (int i = 0; i < count;i++)
+            arr[rand()%size] = rand();
+    }
+    void generateAlreadySortedReversed(){
+        srand(time(nullptr));
+        for (int i = 0; i < size; i++)
+            arr[i] = size-i;
+        int count = rand()%10;
+        for (int i = 0; i < count;i++)
+            arr[rand()%size] = rand();
     }
     void copyTo(Array& second){
         for (int i = 0; i < size; i++)
@@ -204,6 +354,8 @@ public:
             }
         cout << "Count of errors = " << check << endl << endl;
     }
+
+
 };
 
 void demonstration(){
@@ -219,11 +371,53 @@ void demonstration(){
     first.timSort();
     first.output();
     first.check();
-    return;
 };
 
+double timeTimSort(Array<int>& arr){
+    double start = clock();
+    arr.benchTimSort();
+    double end = clock();
+    double time = (end - start) / CLOCKS_PER_SEC;
+    return time;
+}
+double timeLibrarySort(Array<int>& arr){
+    double start = clock();
+    arr.librarySort();
+    double end = clock();
+    double time = (end - start) / CLOCKS_PER_SEC;
+    return time;
+}
+
 void benchmark(){
-    return;
+    int size = 100000;
+    cout << "Array size = " << size << endl;
+    Array<int> workingArray(size), buffer(size);
+    workingArray.generateArray();
+    workingArray.copyTo(buffer);
+
+    cout << "TimSort's time random array - " << timeTimSort(workingArray) << endl;
+    buffer.copyTo(workingArray);
+    cout << "LibrarySort's time random array - " << timeLibrarySort(workingArray) << endl;
+
+    cout << endl;
+
+    workingArray.generateAlreadySorted();
+    workingArray.copyTo(buffer);
+
+    cout << "TimSort's time already sorted array - " << timeTimSort(workingArray) << endl;
+    buffer.copyTo(workingArray);
+    cout << "LibrarySort's time already sorted array - " << timeLibrarySort(workingArray) << endl;
+
+    cout << endl;
+
+    workingArray.generateAlreadySortedReversed();
+    workingArray.copyTo(buffer);
+
+    cout << "TimSort's time already sorted reversed array - " << timeTimSort(workingArray) << endl;
+    buffer.copyTo(workingArray);
+    cout << "LibrarySort's time already sorted reversed array - " << timeLibrarySort(workingArray) << endl;
+
+    cout << endl;
 };
 
 #endif //LAB3C_SOURCE_H
